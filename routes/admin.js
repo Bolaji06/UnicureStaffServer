0fdc49f94d251fbde1d2c6e562c2d644f8947d7b
Bolaji06 <joshspencer0603@gmail.com>
2024-06-06 23:49:54 +0100

    move the admin delete endpoint to admin route

--- routes/admin.js
+++ routes/admin.js
@@ -9,6 +9,31 @@ router.get('/admins', async (req, res) => {
    // console.log(process.)
 });
 
+router.delete("/admin/delete", async (req, res) => {
+    const { email } = req.body;
+    try {
+      await prisma.admin.delete({
+        where: {
+          email: email,
+        },
+      });
+      res.status(200).json({ success: true, message: "admin deleted success" });
+    } catch (err) {
+      if (err.code === "P2025") {
+        res.status(404).json({ success: false, message: "admin not found" });
+      } else {
+        res
+          .status(500)
+          .json({
+            success: false,
+            message: "encounter error while deleting admin",
+          });
+      }
+      console.log(err);
+    } 
+  });
+  
+
 router.get('/cookie', (req, res) => {
     console.log(req.signedCookies);
 })
--- routes/verification.js
+++ routes/verification.js
@@ -29,29 +29,4 @@ router.post("/verify-email", async (req, res) => {
     res.status(500).json({ success: false, message: 'fail to verify email' })
   }
 });
-
-router.delete("/admin/delete", async (req, res) => {
-  const { email } = req.body;
-  try {
-    await prisma.admin.delete({
-      where: {
-        email: email,
-      },
-    });
-    res.status(200).json({ success: true, message: "admin deleted success" });
-  } catch (err) {
-    if (err.code === "P2025") {
-      res.status(404).json({ success: false, message: "admin not found" });
-    } else {
-      res
-        .status(500)
-        .json({
-          success: false,
-          message: "encounter error while deleting admin",
-        });
-    }
-    console.log(err);
-  } 
-});
-
 module.exports = router;
